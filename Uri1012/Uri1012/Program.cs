d0eb312174ac390db985156b084d4518cef784a6
matheus-psd <matheus.p.s.dias@gmail.com>
2024-05-07 23:05:00 -0300

    Add files via upload

---
+++ Uri1012/Uri1012/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+class URI1012
+{
+
+    static void Main(string[] args)
+    {
+        double A, B, C, pi, triangulo, circulo, trapezio, quadrado, retangulo;
+        pi = 3.14159;
+
+        string[] valores = Console.ReadLine().Split(' ');
+        A = double.Parse((valores[0]), CultureInfo.InvariantCulture);
+        B = double.Parse((valores[1]), CultureInfo.InvariantCulture);
+        C = double.Parse((valores[2]), CultureInfo.InvariantCulture);
+
+        triangulo = (A * C) / 2.0;
+        circulo = pi * Math.Pow(C, 2.0);
+        trapezio = ((A + B) * C) / 2.0;
+        quadrado = Math.Pow(B, 2.0);
+        retangulo = A * B;
+
+        Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+    }
+
+}
---
+++ Uri1014/Uri1014/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class URI1014
+{
+
+    static void Main(string[] args)
+    {
+        int dist;
+        double comb, consumo;
+        
+        dist = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        comb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        consumo = dist / comb;
+
+        Console.WriteLine(consumo.ToString("F3") + " km/l");
+
+    }
+
+}
