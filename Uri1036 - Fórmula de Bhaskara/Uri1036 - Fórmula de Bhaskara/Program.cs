5302e9301aec7ad962381271bbd0cea67bc2d872
Matheus Pereira <matheus.p.s.dias@gmail.com>
2024-05-07 23:24:31 -0300

    Add files via upload

---
+++ Uri1036 - Fórmula de Bhaskara/Uri1036 - Fórmula de Bhaskara/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+class URI1036
+{
+
+    static void Main(string[] args)
+    {
+
+        double A, B, C, delta, x1, x2;
+
+        string[] valores = Console.ReadLine().Split(' ');
+        A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+        B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+        C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+        delta = Math.Pow(B, 2.0) - 4.0 * A * C;
+
+
+        if (delta < 0.0 || A == 0) {
+            Console.WriteLine("Impossivel calcular");
+                }
+        else
+        {
+            x1 = (-B + Math.Sqrt(delta)) / (2.0 * A);
+            x2 = (-B - Math.Sqrt(delta)) / (2.0 * A);
+
+            Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
+            Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
+        }
+
+    }
+
+}
---
+++ Uri1037/Uri1037/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+class URI1037
+{
+    static void Main(string[] args)
+    {
+
+        double entrada;
+
+        entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if (entrada >= 0.0 && entrada <= 25.00)
+        {
+            Console.WriteLine("Intervalo [0,25]");
+        }
+        else if (entrada > 25.0 && entrada <= 50.00)
+        {
+            Console.WriteLine("Intervalo (25,50]");
+        }
+        else if (entrada > 50.0 && entrada <= 75.00)
+        {
+            Console.WriteLine("Intervalo (50,75]");
+        }
+        else if (entrada > 75.0 && entrada <= 100.00)
+        {
+            Console.WriteLine("Intervalo (75,100]");
+        }
+        else
+        {
+            Console.WriteLine("Fora de intervalo");
+        }
+    }
+}
