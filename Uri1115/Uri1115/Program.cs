b661a9e4b0ebe05593fcc7b81f47ec55dbed1efb
Matheus Pereira <matheus.p.s.dias@gmail.com>
2024-05-07 23:38:41 -0300

    Add files via upload

---
+++ Uri1115/Uri1115/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class URI1115
+{
+
+    static void Main(string[] args)
+    {
+        int x, y;
+
+        string[] entrada = (Console.ReadLine().Split(' '));
+        x = int.Parse(entrada[0]);
+        y = int.Parse(entrada[1]);
+
+        while (x != 0 && y != 0)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("primeiro");
+                entrada = (Console.ReadLine().Split(' '));
+                x = int.Parse(entrada[0]);
+                y = int.Parse(entrada[1]);
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("segundo");
+                entrada = (Console.ReadLine().Split(' '));
+                x = int.Parse(entrada[0]);
+                y = int.Parse(entrada[1]);
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("terceiro");
+                entrada = (Console.ReadLine().Split(' '));
+                x = int.Parse(entrada[0]);
+                y = int.Parse(entrada[1]);
+            }
+            else
+            {
+                Console.WriteLine("quarto");
+                entrada = (Console.ReadLine().Split(' '));
+                x = int.Parse(entrada[0]);
+                y = int.Parse(entrada[1]);
+            }
+        }
+    }
+
+}
---
+++ Uri1117/Uri1117/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+class URI1117
+{
+
+    static void Main(string[] args)
+    {
+        double nota1, nota2, media;
+
+        nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);         // ler primeira nota
+
+        while (nota1 < 0.0 || nota1 > 10.0)                                             // identificar se é válida
+        {
+            Console.WriteLine("nota invalida");                                         // executar se for inválida
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);     // executar se for inválida
+        }
+
+        nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);         // ler segunda nota
+
+        while (nota2 < 0.0 || nota2 > 10.0)                                             // identificar se é válida
+        {
+            Console.WriteLine("nota invalida");                                         // executar se for inválida
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);     // executar se for inválida
+        }
+
+        media = (nota1 + nota2) / 2.0;                         
+        Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
