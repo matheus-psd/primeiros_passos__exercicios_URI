9a5c3f43543d0ee30d686865cf59981381f9518d
Matheus Pereira <matheus.p.s.dias@gmail.com>
2024-05-07 23:12:30 -0300

    Add files via upload

---
+++ Uri1015/Uri1015/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class URI1015
+{
+
+    static void Main(string[] args)
+    {
+        double x1, x2, y1, y2, dist;
+
+        string[] p1 = Console.ReadLine().Split(' ');
+        x1 = double.Parse((p1[0]), CultureInfo.InvariantCulture);
+        y1 = double.Parse((p1[1]), CultureInfo.InvariantCulture);
+
+        string[] p2 = Console.ReadLine().Split(' ');
+        x2 = double.Parse((p2[0]), CultureInfo.InvariantCulture);
+        y2 = double.Parse((p2[1]), CultureInfo.InvariantCulture);
+
+        dist = Math.Sqrt(Math.Pow(x2 - x1, 2.0) + Math.Pow(y2 - y1, 2.0));
+
+        Console.WriteLine(dist.ToString("F4", CultureInfo.InvariantCulture));
+
+    }
+
+}
---
+++ Uri1016/Uri1016/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class URI1016
+{
+
+    static void Main(string[] args)
+    {
+        int dist, min;
+
+        dist = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        min = dist * 2;
+
+        Console.WriteLine(min + " minutos");
+
+    }
+
+}
