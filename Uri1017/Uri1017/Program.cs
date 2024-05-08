6b43091f2cece2f1947e3023344fdcf0b76f94cd
Matheus Pereira <matheus.p.s.dias@gmail.com>
2024-05-07 23:15:27 -0300

    Add files via upload

---
+++ Uri1017/Uri1017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class URI1017
+{
+
+    static void Main(string[] args)
+    {
+        int tempo, vm;
+        double dist, litros;
+
+        tempo = int.Parse(Console.ReadLine());
+        vm = int.Parse(Console.ReadLine());
+
+        dist = tempo * vm;
+        litros = dist / 12.0;    
+
+        Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+
+    }
+
+}
---
+++ Uri1019/Uri1019/Program.cs
@@ -0,0 +1,9 @@
+﻿int horas, minutos, N, segundos;
+
+N = int.Parse(Console.ReadLine());
+
+horas = N / 3600;
+minutos = (N % 3600) / 60;
+segundos = (N % 3600) % 60;
+
+Console.WriteLine(horas + ":" + minutos + ":" + segundos);
