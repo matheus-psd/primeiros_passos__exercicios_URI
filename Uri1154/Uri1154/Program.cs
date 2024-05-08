c73c7350c9afc7f851a5d86602143056794a402c
Matheus Pereira <matheus.p.s.dias@gmail.com>
2024-05-07 23:41:22 -0300

    Add files via upload

---
+++ Uri1154/Uri1154/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI1154
+{
+
+    static void Main(string[] args)
+    {
+        int entrada, cont;
+        double idade_soma, idade_media;
+
+        entrada = int.Parse(Console.ReadLine());
+        cont = 0;
+        idade_soma = 0;
+
+        while (entrada > 0)
+        {
+            idade_soma += entrada;
+            cont++;
+            entrada = int.Parse(Console.ReadLine());
+        }
+        idade_media = idade_soma / cont;
+        Console.WriteLine(idade_media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
---
+++ Uri1159/Uri1159/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI1159
+{
+
+    static void Main(string[] args)
+    {
+        int x, x2, x3, x4, x5, soma;
+
+        x = int.Parse(Console.ReadLine());
+
+        while (x != 0)
+        {
+            if (x % 2 != 0)
+            {
+                x++;
+            }
+            x2 = x + 2;
+            x3 = x2 + 2;
+            x4 = x3 + 2;
+            x5 = x4 + 2;
+            soma = x + x2 + x3 + x4 + x5;
+
+            Console.WriteLine(soma);
+            x = int.Parse(Console.ReadLine());
+        }
+    }
+}
