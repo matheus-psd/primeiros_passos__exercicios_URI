29d684bbd878d67cae478eabc0a42270afd21a3d
matheus-psd <matheus.p.s.dias@gmail.com>
2024-05-07 23:02:19 -0300

    Add files via upload

---
+++ Uri1010/Uri1010/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI1010
+{
+
+    static void Main(string[] args)
+    {
+        int codigo1, codigo2, qtd1, qtd2;
+        double valor1, valor2, pagar;
+
+        string[] valores = Console.ReadLine().Split(' ');
+        codigo1 = int.Parse(valores[0]);
+        qtd1 = int.Parse(valores[1]);
+        valor1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+        valores = Console.ReadLine().Split(' ');
+        codigo2 = int.Parse(valores[0]);
+        qtd2 = int.Parse(valores[1]);
+        valor2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+        pagar = qtd1 * valor1 + qtd2 * valor2;
+        Console.WriteLine("VALOR A PAGAR: R$ " + pagar.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
---
+++ Uri1011/Uri1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class URI1011
+{
+
+    static void Main(string[] args)
+    {
+        double raio, pi, volume;
+        pi = 3.14159;
+
+        raio = double.Parse(Console.ReadLine());
+
+        volume = (4.0 / 3.0) * pi * Math.Pow(raio, 3.0);
+        
+        Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        
+    }
+
+}
