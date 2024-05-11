3746944750d6692165efb397d6400c5c564ad528
Matheus Pereira <matheus.p.s.dias@gmail.com>
2024-05-11 12:18:21 -0300

    Exercício Uri - Tabuada
    Aprendendo a usar o comando for num exercício Uri.
    O exercício consiste em gerar a tabuada de um número inteiro N a ser lido.

---
+++ Uri1078/Uri1078/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI1018
+{
+
+    static void Main(string[] args)
+    {
+        int N, produto;
+        N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            produto = i * N;
+            Console.WriteLine(i + " x " + N + " = " + produto);
+        }
+
+    }
+
+}
